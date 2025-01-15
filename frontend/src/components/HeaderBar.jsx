78715ec61ddbdb995537504b863185ba5915a17f
Bartosz Warchoł <bartex132@gmail.com>
2025-01-15 21:31:28 +0100

    Alter HeaderBar based on role

--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -4,18 +4,39 @@ import { useUser } from '../UserContext';
 import './HeaderBar.css';
 
 function HeaderBar() {
-  const { token, username, logout } = useUser();
+  const { token, role, username, logout } = useUser();
 
   return (
     <header className="header-bar">
       <div className="left-section">
         <Link to="/" className="header-button">Strona Główna</Link>
-        <Link to="/doctorlist" className="header-button">Lista lekarzy</Link>
+        {role === '' && (
+          <>
+            <Link to="/doctorlist" className="header-button">Lista lekarzy</Link>
+          </>
+        )}
+        {role === 'pacjent' && (
+          <>
+            <Link to="/doctorlist" className="header-button">Lista lekarzy</Link>
+            <Link to="/myappointments" className="header-button">Mój koszyk/zarezerowane wizyty</Link>
+          </>
+        )}
+        {role === 'doctor' && (
+          <>
+            <Link to="/myschedule" className="header-button">Mój harmonogram</Link>
+            <Link to="/manageschedule" className="header-button">Zarządzanie harmonogramem</Link>
+          </>
+        )}
+        {role === 'admin' && (
+          <>
+            <Link to="/doctorlist" className="header-button">Lista lekarzy</Link>
+          </>
+        )}
       </div>
       <div className="right-section">
         {token ? (
           <>
-            <span className="header-username">{username}</span>
+            <span className="header-username">{username} - {role}</span>
             <button className="header-button" onClick={logout}>Logout</button>
           </>
         ) : (
